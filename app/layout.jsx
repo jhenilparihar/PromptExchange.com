ddc03855459b288d0dbcdc91f8f790ae299fe037
Jhenil Parihar <jhenilparihar@gmail.com>
2023-09-12 23:05:00 +0530

    home page updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import "@styles/globals.css";
 
+import Navbar from "@components/Navbar";
+import Provider from "@components/Provider";
+
 export const metadata = {
   title: "PromptExchange",
   description: "Discover & Share AI Prompts",
@@ -12,7 +15,10 @@ const RootLayout = ({ children }) => {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Navbar />
+          {children}
+        </main>
       </body>
     </html>
   );
